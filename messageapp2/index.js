b1db758d023f2b3d5028a1530b4b08b78955e982
Kipngetich222 <123062517+Kipngetich222@users.noreply.github.com>
2025-01-28 15:24:29 +0300

    server adjustments

--- messageapp2/index.js
+++ messageapp2/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const { Server } = require('socket.io')
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
@@ -15,10 +16,12 @@ const server = http.createServer((req, res) => {
     }
 });
 
-const io = require('socket.io')(server);
-const port =5000;
+const io = new Server(server);
+
 
 io.on('connection', (socket) =>{
+    console.log("user connected");
+    
     socket.on('send name', (user) => {
         io.emit('send name', user);
     });
@@ -26,8 +29,14 @@ io.on('connection', (socket) =>{
     socket.on('send message', (chat) => {
         io.emit('send message', chat);
     });
+
+    socket.on('disconnect', ()=> {
+        console.log("user disconnected");
+        
+    })
 });
 
+const port =5000;
 server.listen(port, ()=> {
     console.log(`server listening on port: ${port}`);
     
